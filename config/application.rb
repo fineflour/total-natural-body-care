00997900c6eaf34b15d05ac5d15451471e384eaf
fineflour <soojin.cho@bfa.org>
2017-11-30 15:48:55 -0800

    Add pre-compile assets

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,15 @@ module TotalNaturalBodyCare
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]  
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif .svg .eot .woff .woff2 .ttf)
+    config.autoload_paths += %W(#{config.root}/app/assest/fonts)  
+    config.autoload_paths += %W(#{config.root}/app/assets/images)  
+
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,10 +25,11 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
+   config.assets.precompile = ['*.js', '*.css', '*.scss']
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
