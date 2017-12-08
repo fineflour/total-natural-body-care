1ffdfe729c2694ffff7249adbd402d37ce39c449
fineflour <soojin.cho@bfa.org>
2017-12-07 20:47:40 -0800

    Wip: contact new form added

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,22 @@
+class ContactsController < ApplicationController
+
+  def index
+
+  end
+
+	def new
+		@contact = Contact.new
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def show
+	end
+
+	def destroy
+	end
+end
