8226ad2d271ca856229a48f69745219c45f8c5fc
fineflour <soojin.cho@bfa.org>
2018-03-14 19:02:08 -0700

    WIP:adding animation images

---
+++ app/assets/javascripts/animation.js
@@ -0,0 +1,12 @@
+$(window).load(function(){
+$('img.bgfade').hide();
+var dg_H = $(window).height();
+var dg_W = $(window).width();
+//$('#ani-wrap').css({'height':dg_H,'width':dg_W});
+function anim() {
+    $("#ani-wrap img.bgfade").first().appendTo('#ani-wrap').fadeOut(5000);
+    $("#ani-wrap img").first().fadeIn(5000);
+    setTimeout(anim, 10000);
+}
+anim();})
+//$(window).resize(function(){window.location.href=window.location.href})
