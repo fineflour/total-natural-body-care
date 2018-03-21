95e3a48ec77da58aee74a2c4ceb8c52a0f1c4ca5
fineflour <soojin.cho@bfa.org>
2018-03-20 17:56:09 -0700

    rollback css

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
   root 'home#index'
   resources :articles
   resources :questions, only: [:index, :show, :new, :create, :destroy]
   resources :online_bookings, only: [:index, :show, :new, :create, :edit, :update] 
+
+  get "/pages/:page" => "pages#show"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
