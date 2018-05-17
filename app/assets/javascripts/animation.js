46f3a4aec5f39c3cc77f7edf7e1929e58ca15e5c
fineflour <soojin.cho@bfa.org>
2018-05-17 09:00:45 -0700

    WIP: editing main page

--- app/assets/javascripts/animation.js
+++ app/assets/javascripts/animation.js
@@ -4,9 +4,9 @@ $(window).load(function(){
   var dg_W = $(window).width();
   //$('#ani-wrap').css({'height':dg_H/4,'width':dg_W});
   function anim() {
-    $("#ani-wrap img.bgfade").first().appendTo('#ani-wrap').fadeOut(6000);
-    $("#ani-wrap img").first().fadeIn(5000);
-    setTimeout(anim, 9000);
+    $("#ani-wrap img.bgfade").first().appendTo('#ani-wrap').fadeOut(8000);
+    $("#ani-wrap img").first().fadeIn(7000);
+    setTimeout(anim, 12000);
   }
   anim();})
 //$(window).resize(function(){window.location.href=window.location.href})
