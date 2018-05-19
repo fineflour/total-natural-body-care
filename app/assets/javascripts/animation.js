28718003ef4b3718494e5ceab75359311647c7c4
fineflour <soojin.cho@bfa.org>
2018-05-18 18:47:37 -0700

    change main image size

--- app/assets/javascripts/animation.js
+++ app/assets/javascripts/animation.js
@@ -4,6 +4,7 @@ $(window).load(function(){
   var dg_W = $(window).width();
   //$('#ani-wrap').css({'height':dg_H/4,'width':dg_W});
   function anim() {
+    $("#ani-wrap").first();
     $("#ani-wrap img.bgfade").first().appendTo('#ani-wrap').fadeOut(6000);
     $("#ani-wrap img").first().fadeIn(6000);
     setTimeout(anim, 18000);
